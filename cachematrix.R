a562bf6b7573224744820f0c86b0bde11020817a
ahmedshawky1 <34405473+ahmedshawky1@users.noreply.github.com>
2018-01-20 22:34:42 +0200

    Cache inv Matrix
    Cache inv Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Functions to do a cache for matrix inverse
 
 
-## Write a short comment describing this function
+## function to cache inv matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  invmatrix <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    invmatrix <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInvMatrix <- function(matrix) invmatrix <<- matrix
+  getInvMatrix <- function() invmatrix
+  list(set = set, get = get,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
+  
+  
 }
 
 
-## Write a short comment describing this function
+## Function that checks if we have a cache version or not
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x''
+  
+  #get the Cache version 
+  invmatrix <- x$getInvMatrix()
+  
+  #check if the Cache version is valid 
+  if(!is.null(invmatrix)) {
+    message("getting cached data")
+    return(invmatrix)
+  }
+  
+  #no cached version we have to get the inv matrix
+  data <- x$get()
+  invmatrix <- solve(data, ...)
+  #set the data for next time to reuse
+  x$setInvMatrix(invmatrix)
+  invmatrix
+  
 }
